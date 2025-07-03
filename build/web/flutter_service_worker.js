7c8b8d518de6a8028d7cdddef3762c049843f10b
AlHasssan Balousha <47014491+HassanBalousha@users.noreply.github.com>
2025-07-03 17:15:18 +0300

    Update service worker settings and bootstrap file version

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "60a338ec88cb07292a1487b319a13b09
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "fcdd6f9322a14bb8b08823f76a38313b",
+"flutter_bootstrap.js": "9abed96dd104b47eb8be66bd7317b3b1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
