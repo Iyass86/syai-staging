70755cee97421702870b1b8c351f0441407cbf7a
AlHasssan Balousha <47014491+HassanBalousha@users.noreply.github.com>
2025-07-06 13:35:36 +0300

    Update snap_pixels_page.dart

--- lib/app/ui/pages/snap_pixels_page.dart
+++ lib/app/ui/pages/snap_pixels_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_oauth_chat/app/controllers/snap_controllers/snap_pixels_controller.dart';
 import 'package:flutter_oauth_chat/app/data/models/pixel.dart';
